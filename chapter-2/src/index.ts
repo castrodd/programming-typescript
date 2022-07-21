e43a56de0d4fff11ae58d8dfa1f72d806c385c87
Daniel Castro <castrodd@gmail.com>
2022-07-20 21:06:33 -0700

    initial commit

---
+++ chapter-2/src/index.ts
@@ -0,0 +1,12 @@
+// a is cast to a string 
+let a = 1 + 2 + '3'
+// b remains a string
+let b = a + {}
+let c = {
+    apple: a,
+    banana: b
+}
+// this throws a TypeError
+let d = c.apple * 4
+// this works fine
+let e = Number(c.banana) * 5 
