a670e6c37218670888ec7964adf73aa4656a895a
Daniel Castro <castrodd@gmail.com>
2022-08-23 17:26:19 -0700

    distributive conditionals

--- chapter-6/conditional.ts
+++ chapter-6/conditional.ts
@@ -5,3 +5,20 @@ type IsString<T> = T extends string
 
 type A = IsString<string>  // true
 type B = IsString<number>  // false
+
+// Distributive conditionals
+type ToArray<T> = T[]
+type ANumberArray = ToArray<number>  // number[]
+type ANumberOrStringArray = ToArray<number | string>  // (string | number)[]
+
+type ToArray2<T> = T extends unknown ? T[] : T[]
+type AnotherNumberArray = ToArray2<number>  // number[]
+type AnotherNumOrStringArray = ToArray2<number | string> // number[] | string[]
+
+
+type Without<T, U> = T extends U ? never : T
+
+type WithoutA = Without<
+  boolean | number | string,
+  boolean
+>  // string | number
